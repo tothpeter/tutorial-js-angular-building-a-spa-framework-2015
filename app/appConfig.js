46b72d9076f2545de0a7954cc68d391538863a6f
Peter Toth <tothpeter08@gmail.com>
2016-02-17 22:40:40 +0100

    2.12 Catching exceptions

---
+++ app/appConfig.js
@@ -0,0 +1,8 @@
+angular.modul('app').config(function($provide) {
+  $provide.decorator("$exceptionHandler", ["$delegate", function($delegate) {
+    return function(expception, cause) {
+      $delegate(expception, cause);
+      alert(expception.message);
+    };
+  }])
+})
