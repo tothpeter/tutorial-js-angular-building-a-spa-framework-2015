cecf42b9c33a5afdf90b8f507e52b3682a64f869
Peter Toth <tothpeter08@gmail.com>
2016-02-18 22:15:52 +0100

    Styling the Responsive Menu

--- ext-modules/psFramework/psFrameworkController.js
+++ ext-modules/psFramework/psFrameworkController.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('psFramework').controller('psFrameworkController',
-  ['$scope',
-    function($scope) {
+  ['$scope', '$window', '$timeout',
+    function($scope, $window, $timeout) {
+
+      $scope.isMenuVisible = true;
       $scope.isMenuButtonVisible = true;
 
       $scope.menuButtonClicked = function() {
@@ -11,6 +13,26 @@ angular.module('psFramework').controller('psFrameworkController',
 
       $scope.$on('ps-menu-item-selected-event', function(evt, data) {
         $scope.routeString = data.route;
+      });
+
+      $($window).on('resize.psFramework', function() {
+        $scope.$apply(function() {
+          checkWidth();
+        });
+      });
+
+      $scope.$on("$destroy", function() {
+        $($window).off("resize.psFramework");
+      });
+
+      var checkWidth = function() {
+        var width = $($window).innerWidth();
+        $scope.isMenuVisible = (width > 768-1);
+        $scope.isMenuButtonVisible = !$scope.isMenuVisible;
+      };
+
+      $timeout(function() {
+        checkWidth();
       });
     }
   ]);
