76a5d56a974f8a2a78f5190e31f1ae07513f4724
Peter Toth <tothpeter08@gmail.com>
2016-02-18 16:39:43 +0100

    Making Our Menu Responsive part 1

--- ext-modules/psFramework/psFrameworkController.js
+++ ext-modules/psFramework/psFrameworkController.js
@@ -3,6 +3,12 @@
 angular.module('psFramework').controller('psFrameworkController',
   ['$scope',
     function($scope) {
+      $scope.isMenuButtonVisible = true;
+
+      $scope.menuButtonClicked = function() {
+
+      }
+
       $scope.$on('ps-menu-item-selected-event', function(evt, data) {
         $scope.routeString = data.route;
       });
