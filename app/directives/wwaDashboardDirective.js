dd33137375991f1708f3f106d4fd42ec8fa6ca1a
Peter Toth <tothpeter08@gmail.com>
2016-02-24 18:38:24 +0100

    Dashboard options

--- app/directives/wwaDashboardDirective.js
+++ app/directives/wwaDashboardDirective.js
@@ -7,6 +7,16 @@ angular.module('app').directive('wwaDashboard', [function() {
     template: '<ps-dashboard></ps-dashboard>',
 
     link: function(scope) {
+
+      scope.gridsterOpts = {
+        columns: 12,
+        margins: [20, 20],
+        outerMargin: false,
+        pushing: true,
+        floating: true,
+        swapping: false
+      };
+
       scope.widgets = [
         {
           sizeX: 3,
