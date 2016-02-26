42f9ea4548c24adbc867ccfbe5ae474ff003511b
Peter Toth <tothpeter08@gmail.com>
2016-02-26 21:38:14 +0100

    Compiling widget content

--- app/directives/wwaDashboardDirective.js
+++ app/directives/wwaDashboardDirective.js
@@ -25,15 +25,16 @@ angular.module('app').directive('wwaDashboard', [function() {
           sizeX: 3,
           sizeY: 3,
           row: 0,
-          col: 0
+          col: 0,
+          template: '<wwa-temperature></wwa-temperature>'
         },
-        {
-          title: 'Kalina.tech',
-          sizeX: 2,
-          sizeY: 4,
-          row: 0,
-          col: 5
-        }
+        // {
+        //   title: 'Kalina.tech',
+        //   sizeX: 2,
+        //   sizeY: 4,
+        //   row: 0,
+        //   col: 5
+        // }
       ];
     }
   };
