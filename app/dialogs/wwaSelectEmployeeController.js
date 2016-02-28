daf6b4d1b57c1962f8c19b07a9b85ee3af79d08b
Peter Toth <tothpeter08@gmail.com>
2016-02-28 16:35:55 +0100

    Updating widget settings

--- app/dialogs/wwaSelectEmployeeController.js
+++ app/dialogs/wwaSelectEmployeeController.js
@@ -4,7 +4,22 @@ angular.module('app').controller('wwaSelectEmployeeController',
   [
     '$scope', 'dataService',
     function($scope, dataService) {
+      $scope.isLoaded = false;
 
+      dataService.getEmployees().then(function(employees) {
+        $scope.employees = employees;
+        $scope.isLoaded = true;
+
+        $scope.selectedEmployee = employees.find(function(location) {
+          return location.id === $scope.item.widgetSettings.id;
+        });
+      });
+
+      $scope.saveSettings = function() {
+        $scope.item.widgetSettings.id = $scope.selectedEmployee.id;
+        $scope.$parent.selectedEmployee = $scope.selectedEmployee;
+        $scope.$close();
+      };
     }
   ]
 );
--- app/dialogs/wwaSelectLocationController.js
+++ app/dialogs/wwaSelectLocationController.js
@@ -4,7 +4,22 @@ angular.module('app').controller('wwaSelectLocationController',
   [
     '$scope', 'dataService',
     function($scope, dataService) {
+      $scope.isLoaded = false;
 
+      dataService.getLocations().then(function(locations) {
+        $scope.locations = locations;
+        $scope.isLoaded = true;
+
+        $scope.selectedLocation = locations.find(function(location) {
+          return location.id === $scope.item.widgetSettings.id;
+        });
+      });
+
+      $scope.saveSettings = function() {
+        $scope.item.widgetSettings.id = $scope.selectedLocation.id;
+        $scope.$parent.selectedLocation = $scope.selectedLocation;
+        $scope.$close();
+      };
     }
   ]
 );
