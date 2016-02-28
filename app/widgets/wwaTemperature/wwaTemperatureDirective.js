de1ff0a27fa30b8ee1322b923edc8e041b140e6c
Peter Toth <tothpeter08@gmail.com>
2016-02-28 17:48:14 +0100

    Widget errors

--- app/widgets/wwaTemperature/wwaTemperatureDirective.js
+++ app/widgets/wwaTemperature/wwaTemperatureDirective.js
@@ -8,11 +8,24 @@ angular.module('app').directive('wwaTemperature',
         templateUrl: 'app/widgets/wwaTemperature/wwaTemperatureTemplate.html',
         link: function(scope, el, attrs) {
           scope.isLoaded = false;
+          scope.hasError = false;
 
-          dataService.getLocation(scope.item.widgetSettings.id).then(function(data) {
-            scope.selectedLocation = data;
-            scope.isLoaded = true;
-          });
+          scope.loadLocation = function() {
+            scope.hasError = false;
+
+            dataService.getLocation(scope.item.widgetSettings.id).then(
+              function(data) {
+                scope.selectedLocation = data;
+                scope.isLoaded = true;
+                scope.hasError = false;
+              },
+              function(data) {
+                scope.hasError = true;
+              }
+            );
+          };
+
+          scope.loadLocation();
         }
       };
     }
