6b2484d2753e2f6cf43d60e87d9e3af3ce920124
Peter Toth <tothpeter08@gmail.com>
2016-02-28 15:02:33 +0100

    Turn of alert for exception

--- app/appConfig.js
+++ app/appConfig.js
@@ -2,7 +2,7 @@ angular.module('app').config(function($provide) {
   $provide.decorator("$exceptionHandler", ["$delegate", function($delegate) {
     return function(expception, cause) {
       $delegate(expception, cause);
-      alert(expception.message);
+      // alert(expception.message);
     };
   }])
 })
